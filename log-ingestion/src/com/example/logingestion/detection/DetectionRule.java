cb4c84064b440165384356416ed90d63622daf84
swathi-2106 <162590527+swathi-2106@users.noreply.github.com>
2026-05-08 14:49:27 +0530

    Delete log-ingestion directory

--- log-ingestion/src/com/example/logingestion/detection/DetectionRule.java
+++
@@ -1,8 +0,0 @@
-package com.example.logingestion.detection;
-
-import com.example.logingestion.LogEntry;
-import java.util.List;
-
-public interface DetectionRule {
-    List<Alert> apply(List<LogEntry> logs);
-}
